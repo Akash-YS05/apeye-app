7b3fa765c145f14b6972a6dd4d133fa68e127845
Akash-YS05 <akash.pan05@gmail.com>
2026-01-12 15:35:32 +0530

    created user repository

---
+++ apeye-backend/internal/repository/user_repository.go
@@ -0,0 +1,63 @@
+package repository
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/Akash-YS05/apeye-app/apeye-backend/internal/models"
+	"gorm.io/gorm"
+)
+/*
+	user repository is a layer that talks to the database so the rest of the app doesnt need to know SQL or GORM details
+*/
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+//constructor - creates a repo instance
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Create(user *models.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *UserRepository) FindByEmail(email string) (*models.User, error) {
+	var user models.User	
+	err := r.db.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil		//user not found
+		}
+		return nil, err		//db error
+	}
+	return &user, nil		//user found
+}
+
+func (r *UserRepository) FindByID(id uuid.UUID) (*models.User, error) {
+	var user models.User
+	err := r.db.First(&user, "id = ?", id).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) Update(user *models.User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *UserRepository) Delete(id uuid.UUID) error {
+	return r.db.Delete(&models.User{}, "id = ?", id).Error
+}
+
+func (r *UserRepository) EmailExists(email string) (bool, error) {
+	var count int64
+	err := r.db.Model(&models.User{}).Where("email = ?", email).Count(&count).Error
+	return count > 0, err
+}
