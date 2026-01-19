a2b8addcf59e61d100f202067fcffea6a88c21e9
Akash-YS05 <akash.pan05@gmail.com>
2026-01-19 15:55:52 +0530

    added resizable option for response viewer

--- apeye-frontend/src/components/features/layout/main-layout.tsx
+++ apeye-frontend/src/components/features/layout/main-layout.tsx
@@ -6,33 +6,47 @@ import Header from './header';
 import RequestBuilder from '../request-builder/RequestBuilder';
 import ResponseViewer from '../response-viewer/ResponseViewer';
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
+
 export default function MainLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
     <div className="h-screen flex flex-col overflow-hidden">
-      {/* Header */}
       <Header onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
 
       <div className="flex flex-1 overflow-hidden">
-        {/* Sidebar */}
         {sidebarOpen && (
           <div className="w-64 border-r bg-gray-50 dark:bg-gray-900 overflow-auto">
             <Sidebar />
           </div>
         )}
 
-        {/* Main Content */}
-        <div className="flex-1 flex flex-col overflow-hidden">
-          {/* Request Builder - 30% of space, compact */}
-          <div className="h-[30vh] border-b overflow-auto flex-shrink-0">
-            <RequestBuilder />
-          </div>
+        {/* MAIN CONTENT */}
+        <div className="flex-1 overflow-hidden">
+          <ResizablePanelGroup direction="vertical" className="h-full">
+            
+            {/* Request Builder */}
+            <ResizablePanel defaultSize={30} minSize={15}>
+              <div className="h-full overflow-auto border-b">
+                <RequestBuilder />
+              </div>
+            </ResizablePanel>
 
-          {/* Response Viewer - 70% of space */}
-          <div className="flex-1 overflow-hidden">
-            <ResponseViewer />
-          </div>
+            <ResizableHandle withHandle />
+
+            {/* Response Viewer */}
+            <ResizablePanel defaultSize={70} minSize={20}>
+              <div className="h-full overflow-hidden">
+                <ResponseViewer />
+              </div>
+            </ResizablePanel>
+
+          </ResizablePanelGroup>
         </div>
       </div>
     </div>
